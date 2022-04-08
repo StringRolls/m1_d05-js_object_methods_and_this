6f37a873158f2e95234284f1431bcfd2a5c3b9a0
TA WDFT BCN Ironhack <ta-web-bcn@ironhack.com>
2022-04-08 13:45:19 +0200

    solution

--- exercises/01-exe-js_objects_and_this.js
+++ exercises/01-exe-js_objects_and_this.js
@@ -1,44 +1,41 @@
-// TODO: write the methods getAge, addFriend and getRandomFriend
+// TODO: write the methods getAge, addJoke and getRandomJoke
 
 const chuck = {
-    firstName: 'Chuck',
-    lastName: 'Norris',
-    birthDate: new Date('1940-03-10'),
-    friends: [
-      'Pablo',
-      'Julie',
-      'Yanis'
-    ],
-    displayInfo: function() {
-      console.log(
-        `My name is ${this.firstName} ${this.lastName} and I have ${this.friends.length} friends.`
-      );
-    },
-    getAge: function() {
-      // TODO: return the age in this function
-      // Hint: to get the current time, you can do: new Date()
-      // Hint: to get the birthDate, you can do: this.birthDate
-      // Hint: you can subtract 2 dates and you get the number of milliseconds
-      // Hint: convert the milliseconds to years 
-    },
-    addFriend: function(name) {
-      // TODO (don't use return statement)
-      // add a name to the friends array. The friend name is passed as a parameter to the function
-    },
-    getRandomFriend: function() {
-      // TODO. return a random friend name from the friends array
-    }
-  };
-  
-  chuck.displayInfo();
-  
-  console.log('getAge', chuck.getAge()); // Should return 81 if you are in 2021
-  
-  chuck.addFriend('Jorge');
-  console.log('getRandomFriend', chuck.getRandomFriend());
-  chuck.addFriend('Helena');
-  console.log('getRandomFriend', chuck.getRandomFriend());
-  chuck.addFriend('Manish');
-  console.log('getRandomFriend', chuck.getRandomFriend());
-  
-  chuck.displayInfo();
+  firstName: 'Chuck',
+  lastName: 'Norris',
+  birthDate: new Date('1940-03-10'),
+  jokes: [
+    'Chuck Norris counted to infinity... Twice.',
+    'Chuck Norris is the only man to ever defeat a brick wall in a game of tennis'
+  ],
+  displayInfo() {
+    console.log(
+      `My name is ${this.firstName} ${this.lastName} and I have ${this.jokes.length} jokes.`
+    );
+  },
+  getAge() {
+    return Math.floor(
+      (new Date() - this.birthDate) / 1000 / 60 / 60 / 24 / 365.25
+    );
+  },
+  addJoke(joke) {
+    this.jokes.push(joke);
+  },
+  getRandomJoke() {
+    let randomIndex = Math.floor(Math.random() * this.jokes.length);
+    return this.jokes[randomIndex];
+  }
+};
+
+chuck.displayInfo();
+
+console.log('getAge', chuck.getAge());
+
+chuck.addJoke('Chuck Norris can divide by zero.');
+console.log('getRandomJoke', chuck.getRandomJoke());
+chuck.addJoke('Chuck Norris kills flies with his gun.');
+console.log('getRandomJoke', chuck.getRandomJoke());
+chuck.addJoke('Chuck Norris was once in a knife fight, and the knife lost.');
+console.log('getRandomJoke', chuck.getRandomJoke());
+
+chuck.displayInfo();
